e371cb91da60c02505ec3e9a0b7da636d077a55b
MiHa0_o <MiHa0_o@209-5>
2024-11-18 12:52:28 +0200

    Добавьте файлы проекта.

---
+++ lab4.2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите m");
+int m = int.Parse(Console.ReadLine());
+int[,] mas =  new int[n, m];
+Console.WriteLine();
+for (int i = 0; i < mas.GetLength(0); i++)
+{
+    int max_row = int.MinValue, min_row = int.MaxValue;
+    for (int j = 0; j < mas.GetLength(1); j++)
+    {
+        if (i == 0 || j==0) { mas[i, j] = 1; }
+        else { mas[i, j] = mas[i - 1, j] + mas[i, j - 1]; }
+        Console.Write(mas[i, j] + " ");
+    }
+    Console.WriteLine();
+}
